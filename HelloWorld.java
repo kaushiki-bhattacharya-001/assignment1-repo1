1f09e1be521f3963db4e84896cb9601d86c9ec01
kaushiki.bhattacharya@sap.com <kaushiki.bhattacharya@sap.com>
2023-03-10 21:14:59 +0530

    Fourth Commit to Hotfix

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String args[]) { 
 
 		System.out.println("HELLO WORLD!");
-		System.out.println("20 + 30 = "+(20+30));
+		String str1 = "Kaushiki", str2 = "Bhattacharya";
+		System.out.println("String Concatenation: "+(str1 + " " + str2));
 
 	}
 
